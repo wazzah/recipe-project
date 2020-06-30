44d2a0388f81fbd2c585a4674604f8cbc04ff8e2
Warren Howes <warren.howes@hotmail.co.uk>
2020-06-30 10:55:14 +0000

    added a category selector

--- app.py
+++ app.py
@@ -19,7 +19,8 @@ def get_tasks():
 
 @app.route('/add_recipe')
 def add_recipe():
-    return render_template('addrecipe.html')
+    return render_template('addrecipe.html',
+    categories=mongo.db.categories.find())
 
 
 
