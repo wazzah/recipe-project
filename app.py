3b40b495cf4997b8627bce11e73ac6542336866d
Warren Howes <warren.howes@hotmail.co.uk>
2020-06-30 15:05:14 +0000

    added the editrecipe.html file

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId 
+
 if os.path.exists("env.py"):
     import env
 
@@ -29,11 +30,15 @@ def insert_recipe():
     tasks.insert_one(request.form.to_dict)
     return redirect(url_for('gets_tasks'))
 
-
-
+@app.route('/edit_recipe/<task_id>')
+def edit_recipe(task_id):
+    the_task =  mongo.db.tasks.find_one({"_id": ObjectId(task_id)})
+    all_categories =  mongo.db.categories.find()
+    return render_template('editrecipe.html', task=the_task,
+                           categories=all_categories    
 
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
-            port=int(os.environ.get('PORT')),
-            debug=True)
+        port=int(os.environ.get('PORT')),
+        debug=True)
